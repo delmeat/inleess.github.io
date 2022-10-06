d964d71c4a38579476f27f2eaafce7b93f8e00a5
delmeat <ruslan100502@gmail.com>
2022-10-06 16:53:44 +0600

    pokazat site

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('.slider_images-header').slick({
+  dots: true,
+  prevArrow: '<button type="button" class="slick-prev"></button>',
+  nextArrow: '<button type="button" class="slick-next"></button>',
+  infinite: true,
+  speed: 500,
+  fade: false,
+  cssEase: 'linear',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+});
